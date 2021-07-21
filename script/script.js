e99681cc1d574a69f207df3aaf8e8424170a4a6f
Ben Wheatley <Info@wheatleystudios.com>
2021-07-21 22:59:46 +0100

    Layout of main page completed

--- script/script.js
+++ script/script.js
@@ -1,15 +1,30 @@
 
-
-var className = "inverted";
-var scrollTrigger = 60;
+let scrollTrigger = 200;
+let scrollTriggerTwo = 1450;
+let scrollTriggerThree = 2800;
+let scrollTriggerFour = 4800;
 
 window.onscroll = function() {
-  // We add pageYOffset for compatibility with IE.
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    document.getElementById('headerContainer').style.backgroundColor = '#E8A69D';
-  } else {
+    if (window.scrollY >= scrollTriggerFour){
+        document.getElementById('heroImage').src= "../OnlineStoreFullStack/assets/logos/socialBanner.png";
+        
+    } 
+    else if (window.scrollY >= scrollTriggerThree){
+        document.getElementById('heroImage').src= "../OnlineStoreFullStack/assets/products/heroBanners/threeTierOffice.png";
+        
+
+    } else if (window.scrollY >= scrollTriggerTwo){
+        document.getElementById('heroImage').src= "../OnlineStoreFullStack/assets/products/heroBanners/threeTierDinning.png";
+
+    } else if (window.scrollY >= scrollTrigger) {
+        document.getElementById('headerContainer').style.backgroundColor = '#E8A69D';
+        document.getElementById('logoBlack').style.display = 'none';
+        document.getElementById('logoWhite').style.display = 'block';
+        document.getElementById('heroImage').src= "../OnlineStoreFullStack/assets/products/heroBanners/twoTierWhite.png";
+    }
+     else {
     document.getElementById('headerContainer').style.backgroundColor = 'transparent';
+    document.getElementById('logoBlack').style.display = 'block';
+    document.getElementById('logoWhite').style.display = 'none';  
   }
 };
-
-
